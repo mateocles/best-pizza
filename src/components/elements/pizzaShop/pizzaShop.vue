0f99cbb1e4cc4d79c44274ba10d722e85a8c7285
Mateo Ceballos Bermudez <mat.bermudez@udla.edu.co>
2021-05-18 00:35:33 -0500

    feature: a card of the pizza shops has been added

--- src/components/elements/pizzaShop/pizzaShop.vue
+++ src/components/elements/pizzaShop/pizzaShop.vue
@@ -7,27 +7,18 @@
       <span>Escoge tu pizzería favorita</span>
     </div>
     <div>
-      <b-card
-        title="Title"
-        img-src="https://picsum.photos/300/300/?image=41"
-        img-alt="Image"
-        img-top
-      >
-        <b-card-text>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </b-card-text>
-        <template #footer>
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </template>
-      </b-card>
+      <CardShop />
     </div>
   </div>
 </template>
 
 <script>
+import CardShop from "../card/card";
 export default {
   name: "pizza-shop",
+  components: {
+    CardShop,
+  },
 };
 </script>
 
